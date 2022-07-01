ed2906bacd3c73955548e56b172c105c82b676ac
Harry <greatd24@icloud.com>
2022-07-01 15:26:03 +0800

    ship can fired

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings:
         #屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 150)
+        self.bg_color = (128, 128, 128)
 
         #飞船设置
         self.ship_speed = 1.5
@@ -15,4 +15,5 @@ class Settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (0, 0, 0)
+        self.bullet_allowed = 3
